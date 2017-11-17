7addfaed319a70faf79843baed262953b7d2e6ea
CJTSAJ <997149731@qq.com>
2017-11-17 15:28:50 +0800

    linux环境出问题，将代码迁移到windows。

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,35 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+//let yeomanImage = require('../images/yeoman.png');
+
+//获取图片数据
+var imageDatas = require('../data/imagedata.json');
+
+//将图片名信息转为URL信息
+function genImageURL(imageDataArr){
+  for(var i = 0, j = imageDataArr.lenth; i < j; i++){
+    var singleImageData = imageDataArr[i];
+
+    singleImageData.imageURL = require('../images/' + singleImageData.filename);
+    imageDataArr[i] = singleImageData;
+  }
+  return imageDataArr;
+}
+
+imageDatas = genImageURL(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+          <section className="img-sec">
+          </section>
+          <nav className="controller-nav">
+          </nav>
+      </section>
     );
   }
 }
